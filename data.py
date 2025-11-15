a81a93f270d7052b9149f97e869186b96799b97d
gvv2065 <vparadise@ya.ru>
2025-11-15 11:50:49 +0300

    test_create_existed_user, test_create_user_missing_required_field

--- data.py
+++ data.py
@@ -3,3 +3,7 @@ class Url:
     BASE_API = "https://stellarburgers.education-services.ru/api"
     INGREDIETNS = BASE_API  + "/ingredients"
     REGISTER = BASE_API  + "/auth/register"
+    
+class User:
+    EMAIL = "gvvtest@ya.ru"
+    PASSWORD = "pwdpwdpwd"
